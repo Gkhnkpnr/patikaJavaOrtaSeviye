fffed6fe7f2c370562530bc5dbde6c2c7589e627
Gökhan Akpınar <gkhnkpnr@icloud.com>
2022-04-15 01:52:38 +0300

    ThreadRace Yuklendi

---
+++ Java 102/threadRace/threadRace/src/EvenNumbers.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class EvenNumbers extends Thread{
+    public ArrayList<Integer> tempList;
+
+    ArrayList<Integer> evenList = new ArrayList<>();
+
+    public EvenNumbers(ArrayList<Integer> tempList){
+        this.tempList = tempList;
+    }
+
+    @Override
+    public void run() {
+        for (int i =0; i< tempList.size(); i++){
+            if (tempList.get(i) % 2 == 0){
+                evenList.add(tempList.get(i));
+            }
+        }
+        print();
+    }
+
+    public void print(){
+        System.out.println(evenList.toString());
+    }
+}
